eb7aa77b186d7f9a569cbae974d328af99c0143b
Tuomas Mäkinen <tuomas.makinen@alfame.com>
2020-11-17 10:38:51 +0200

    Wrap action code in async

--- index.js
+++ index.js
@@ -3,35 +3,37 @@ const github = require('@actions/github');
 const { Octokit } = require('@octokit/core');
 const write = require('write');
 
-try {
-	const owner = core.getInput('owner');
-	const repo = core.getInput('repo');
-	const tag = core.getInput('tag');
-	const filename = core.getInput('filename');
-	const token = core.getInput('token');
-	const output = core.getInput('output');
+async function run() {
+	try {
+		const owner = core.getInput('owner');
+		const repo = core.getInput('repo');
+		const tag = core.getInput('tag');
+		const filename = core.getInput('filename');
+		const token = core.getInput('token');
+		const output = core.getInput('output');
 
-	const oktokit = new Octokit(token ? { auth: token } : null);
+		const oktokit = new Octokit(token ? { auth: token } : null);
 
-	const release = await octokit.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
-		owner,
-		repo,
-		tag
-	});
+		const release = await octokit.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+			owner,
+			repo,
+			tag
+		});
 
-	const asset = release.assets.find(asset => {
-		return asset.name === filename;
-	});
+		const asset = release.assets.find(asset => {
+			return asset.name === filename;
+		});
 
-	const binary = await octokit.request('GET /repos/{owner}/{repo}/releases/{asset_id}', {
-		owner,
-		repo,
-		asset_id: asset.id
-	});
+		const binary = await octokit.request('GET /repos/{owner}/{repo}/releases/{asset_id}', {
+			owner,
+			repo,
+			asset_id: asset.id
+		});
 
-	core.setOutput('content', binary);
+		core.setOutput('content', binary);
 
-	write.sync(output ? output : filename, binary, { newline: true });
-} catch (error) {
-	core.setFailed(error.message);
+		write.sync(output ? output : filename, binary, { newline: true });
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
